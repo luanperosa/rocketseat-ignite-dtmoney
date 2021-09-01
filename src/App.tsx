ac96bfb3d26392ae0c1b590c675aceacae108619
Luan Perosa Chitto <luan.perosa@hotmail.com>
2021-09-01 01:43:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement("#root");
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+  
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider >
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
